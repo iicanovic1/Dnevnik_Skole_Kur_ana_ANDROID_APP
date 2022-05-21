f1ae8c56596dac35bd0e580f052484071c7fb64d
iicanovic1 <73068169+iicanovic1@users.noreply.github.com>
2022-05-21 08:34:43 +0200

    keširanje studenata sa NetworkBoundResource

---
+++ app/src/main/java/com/example/dnevnikskolekur_ana/other/NotworkBoundResource.kt
@@ -0,0 +1,32 @@
+ package com.example.dnevnikskolekur_ana.other
+
+import kotlinx.coroutines.flow.*
+           // rezultat  // zahtjev -response (List<Student>)
+inline fun <ResultType, RequestType> networkBoundResource( // vraća flow
+    crossinline query: () -> Flow<ResultType>,// REZULTAT // dobavljanje iz loklane baze
+    crossinline fetch: suspend () -> RequestType,// ZAHTJEV // dobavljanje sa API - ja
+    crossinline saveFetchResult: suspend (RequestType) -> Unit, // ZAHTJEV // spašavanje što smo dobili sa APi-ja u bazu
+    crossinline onFetchFailed: (Throwable) -> Unit = { Unit }, // pomaže šta da radimo kad ne uspije fetch sa API-ja
+    crossinline shouldFetch: (ResultType) -> Boolean = {true} // REZULTAT // prima iz Baze i određeuje hoćemo li to ili ćemo fetch sa API
+) = flow {
+    emit(Resource.loading(null))  // kada se pozove ova funkcija Live Data bude trigerovan
+    val data = query().first() // dobavljanje podataka iz baze - flow liste studenata
+
+    val flow = if(shouldFetch(data)){ // hoćemo fetch sa servera da pokušamo
+        emit(Resource.loading(data))  // možemo trenutno pokazati iz baze što imamo sad za sad
+
+        try {
+            val fetcedResult = fetch() // Fetch sa API
+            saveFetchResult(fetcedResult)  // spašavanje u bazu
+            query().map { Resource.success(it)} // pozivanje iz baze i smještanje u resurs
+        }catch (t  : Throwable){
+            onFetchFailed(t)
+            query().map {
+                Resource.error("Nije moguće kontaktirati server!", it)
+            } // opet će uzeti iz baze ali ono što je već prije  bilo bez update sa API -ja
+        }
+    }else{
+        query().map {Resource.success (it)} // svakako nećeš sa API , uzmi iz baze samo
+    }
+    emitAll(flow)
+}
--- app/src/main/java/com/example/dnevnikskolekur_ana/repositories/StudentRepository.kt
+++ app/src/main/java/com/example/dnevnikskolekur_ana/repositories/StudentRepository.kt
@@ -2,10 +2,14 @@ package com.androiddevs.ktornoteapp.repositories
 
 import android.app.Application
 import com.example.dnevnikskolekur_ana.data.local.StudentDao
+import com.example.dnevnikskolekur_ana.data.local.entities.Student
 import com.example.dnevnikskolekur_ana.data.remote.StudentApi
 import com.example.dnevnikskolekur_ana.data.remote.requests.AccountRequest
 import com.example.dnevnikskolekur_ana.other.Resource
+import com.example.dnevnikskolekur_ana.other.checkForInternetConnection
+import com.example.dnevnikskolekur_ana.other.networkBoundResource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 import javax.inject.Inject
@@ -40,5 +44,24 @@ class StudentRepository @Inject constructor(
         }catch (e: Exception){
             Resource.error("Nije moguće kontaktirati server. Provjerite internet konekciju!",null)
         }
+    }
+
+    fun getAllStudents() : Flow<Resource<List<Student>>> {
+        return networkBoundResource(
+                query = {
+                    studentDao.getAllStudents()
+                },
+                fetch = {
+                    studentApi.getStudnets()
+                },
+                saveFetchResult = { response ->   // što smo dobili sa api-ja
+                    response.body()?.let{
+                        //insertNotes(it.onEach { note -> note.isSynced = true })
+                    }
+                },
+                shouldFetch = {
+                    checkForInternetConnection(context)
+                }
+        )
     }
 }
